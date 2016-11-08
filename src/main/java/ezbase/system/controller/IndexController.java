aff499892fd42f617fb6a83b0a981f84a9b7ae48
leezhao <939289585@qq.com>
2016-11-08 22:53:50 +0800

    config spring mvc

--- src/main/java/ezbase/system/controller/IndexController.java
+++ src/main/java/ezbase/system/controller/IndexController.java
@@ -3,11 +3,20 @@
  **/
 package ezbase.system.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * <p>初始化</p>
  * @author LeeZhao
  * @date 16/10/11
  * @version 1.0
  **/
+@Controller
 public class IndexController {
+
+    @RequestMapping(value = "home")
+    private String index(){
+        return "login";
+    }
 }
